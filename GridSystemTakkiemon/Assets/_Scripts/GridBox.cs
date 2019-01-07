b7b9d324b9358b0ebcf1e5ed862a43b24e5b2067
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-01-07 18:01:30 +0100

    overhaul entire project to get rid of errors and add gridpoint and gridbox scripts

---
+++ GridSystemTakkiemon/Assets/_Scripts/GridBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBox : MonoBehaviour
+{
+    public GridPoint gridPoint; //this holds the location of the gridbox in the form of three coordinates (x, y and z)
+    public float size; //this is mostly the distance between the centre of the hex and a corner/side(tbd)
+
+    public int DistanceToGridBox(GridPoint comparingGridBox)
+    {
+        return gridPoint.
+    }
+}
---
+++ GridSystemTakkiemon/Assets/_Scripts/GridPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPoint : MonoBehaviour
+{
+    public int[] coordinates = new int[3]; //this array holds the three coördinates of the hexgridbox: x, y and z
+
+    public int x() => this.coordinates[0];
+    public int y() => this.coordinates[1];
+    public int z() => this.coordinates[2];
+
+    public int DistanceToGridPoint(GridPoint comparingGridPoint)
+    {
+        int[] comparingValues = new int[3];
+        for (int i = 0; i < comparingValues.Length; i++)
+        {
+            comparingValues[i] = Math.Abs(comparingGridPoint.coordinates[i] - this.coordinates[i]);
+        }
+        return (comparingValues.Sum() / 2);
+    }
+}
