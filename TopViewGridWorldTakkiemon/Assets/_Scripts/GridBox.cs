b2f827fd4dbeb2d2f4608bb9aa79a05de1ed776f
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-01-23 18:59:26 +0100

    add hovering mechanic over gridboxes

--- TopViewGridWorldTakkiemon/Assets/_Scripts/GridBox.cs
+++ TopViewGridWorldTakkiemon/Assets/_Scripts/GridBox.cs
@@ -6,9 +6,20 @@ public class GridBox : MonoBehaviour
 {
     public GridPoint gridPoint; //this holds the location of the gridbox in the form of three coordinates (x, y and z)
     public float gridBoxSize; //this is mostly the distance between the centre of the hex and a corner/side(tbd)
+    public bool isSelected;
 
     public int DistanceToGridBox(GridBox comparingGridBox)
     {
         return gridPoint.DistanceToGridPoint(comparingGridBox.gridPoint);
+    }
+
+    public void OnMouseEnter()
+    {
+        //change color
+    }
+
+    public void OnMouseExit()
+    {
+        //unchange color
     }
 }
